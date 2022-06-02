5a4c9c4c6766570ed5cfddbae8151289bebee9bd
Olli-Antti Kivilahti <olli-antti.kivilahti@hypernova.fi>
2022-06-03 01:51:59 +0300

    More simple release tool with well-known dependencies.

---
+++ pack.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+# Copyright 2019 Magnus Enger, Libriotech <magnus@libriotech.no>
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this; if not, see <http://www.gnu.org/licenses>.
+
+=pod
+=head1 pack.pl
+Package up the relevant parts as a .kpz Koha plugin file.
+=head1 USAGE
+  sudo koha-shell -c "perl pack.pl" <kohainstance>
+=head1 PREREQUISITES
+=head2 zip
+  sudo apt-get install zip
+=cut
+
+use Modern::Perl;
+use lib '.';
+
+use Koha::Plugin::Fi::KohaSuomi::SelfService;
+my $version  = $Koha::Plugin::Fi::KohaSuomi::SelfService::VERSION;
+my $filename = "/tmp/koha-plugin-self-service-$version.kpz";
+
+say `zip -r $filename Koha/`;
+
+if ( -f $filename ) {
+    say "$filename created";
+} else {
+    say "Oooops, something went wrong!";
+}

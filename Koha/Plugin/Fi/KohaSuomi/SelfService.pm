317ae1ab35a2600574543ac8060faf5fed87c295
Olli-Antti Kivilahti <olli-antti.kivilahti@hypernova.fi>
2022-06-10 23:28:04 +0300

    Remove /borrowers/status as it conflicts with other kohasuomi plugins.

--- Koha/Plugin/Fi/KohaSuomi/SelfService.pm
+++ Koha/Plugin/Fi/KohaSuomi/SelfService.pm
@@ -52,7 +52,7 @@ use Koha::Plugin::Fi::KohaSuomi::SelfService::Exception::PermissionRevoked;
 use Koha::Plugin::Fi::KohaSuomi::SelfService::Exception::TACNotAccepted;
 use Koha::Plugin::Fi::KohaSuomi::SelfService::Exception::Underage;
 
-our $VERSION = "1.0.6";
+our $VERSION = "1.0.7";
 
 our $metadata = {
     name            => 'Koha Self Service Permission API',
